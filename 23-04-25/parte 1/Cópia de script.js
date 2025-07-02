7b07f8704b5a78d5feaeb7dad108c0e8b975f915
llucasprogramador <supersupermasterlucas@gmail.com>
2025-07-02 08:37:35 -0300

    Abril

---
+++ 23-04-25/parte 1/Cópia de script.js
@@ -0,0 +1,6 @@
+// Exibe "Hello World!" no console do navegador
+console.log("Hello world!");
+
+// Exemplo de interação com o usuário
+let nome = prompt("Qual é o seu nome?"); // Pede
+console.log("Olá, "+ nome + "!"); // Exibe uma
---
+++ 23-04-25/parte 2/js/script.js
@@ -0,0 +1,10 @@
+var anoAtual = 2025;
+
+
+var anoNascimento = prompt ("digite o ano que voce nasceu")
+
+
+var idade = anoAtual - anoNascimento;
+
+
+alert("Sua idade é: " + idade + " anos")
